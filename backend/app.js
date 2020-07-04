f034b629b10fcf2acae4294af064d66274bba1ec
Christopher Vouga <crvouga@gmail.com>
2020-07-04 16:40:00 -0700

    serve react app from express

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const buildPath = path.join(__dirname, "..", "frontend", "build");
+
+app.use(express.static(buildPath));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
 app.use("/tmdb", require("./tmdb"));
