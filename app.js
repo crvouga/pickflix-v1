fb9393b572e52f0ad00e7e00f0541dc22315e0cb
Christopher Vouga <crvouga@gmail.com>
2020-07-06 21:05:27 -0700

    FIXED client routing refresh bug

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const buildPath = path.join(__dirname, "client", "build");
-app.use(express.static(buildPath));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(buildPath, "index.html"));
-});
-
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(buildPath, "index.html"));
-// });
-
 app.use("/api/tmdb", require("./tmdb"));
 app.use("/api/youtube", require("./youtube"));
+
+const buildPath = path.join(__dirname, "client", "build");
+app.use(express.static(buildPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
