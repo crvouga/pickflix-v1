e506c8eca384c4cafbd430d0ae816bba36eee233
Christopher Vouga <crvouga@gmail.com>
2020-07-13 19:51:09 -0700

    🔧FIX video playing while modal not open

--- client/src/app/configureStore.js
+++ client/src/app/configureStore.js
@@ -1,10 +1,16 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
-import { fork } from "redux-saga/effects";
+import { fork, select, put, takeEvery } from "redux-saga/effects";
 import player from "../video/redux/player";
 import modal from "../common/redux/modal";
 
 function* rootSaga() {
+  yield takeEvery(player.actions.play, function* () {
+    const isVideoModalOpen = yield select(modal.selectors.isOpen("videoModal"));
+    if (!isVideoModalOpen) {
+      yield put(player.actions.pause());
+    }
+  });
   yield fork(player.saga);
 }
 
