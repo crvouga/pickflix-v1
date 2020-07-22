47e8486efa14995427ee27e361f38e8314fb4348
Christopher Vouga <crvouga@gmail.com>
2020-07-22 14:57:58 -0700

    👮‍♂️🐛handle undefined origin

--- backend/cors.js
+++ backend/cors.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 
-const clientOrigin = `https://pickflix.web.app/`;
+const clientOrigin = `https://pickflix.web.app`;
 
 const env = process.env.NODE_ENV || "development";
 
@@ -8,20 +8,26 @@ module.exports = () =>
   cors({
     origin: (origin, callback) => {
       /** allow access if in dev mode */
-      console.log({ env });
       if (env === "development") {
         return callback(null, true);
       }
 
-      console.log({ origin, clientOrigin });
-      /**  allow access if client is calling api */
-      const originHostname = new URL(origin).hostname;
-      const clientHostname = new URL(clientOrigin).hostname;
-      console.log({ originHostname, clientHostname });
-
-      if (originHostname === clientHostname) {
+      /** Assuming an undefined origin is from from browser then allow access if origin is from browser */
+      if (!origin) {
         return callback(null, true);
       }
+
+      try {
+        const originHostname = new URL(origin || "").hostname;
+        const clientHostname = new URL(clientOrigin).hostname;
+        /**  allow access if client is calling api */
+        if (originHostname === clientHostname) {
+          return callback(null, true);
+        }
+      } catch (error) {
+        return callback(new Error(error));
+      }
+
       return callback(new Error("Blocked by CORS"));
     },
   });
