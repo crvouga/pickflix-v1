98931113808ecaf0abd9d1cc924270e3058c9c0d
Christopher Vouga <crvouga@gmail.com>
2020-09-21 15:06:21 -0700

    gets aggergated list

--- backend/src/lists/logic/get-lists/logic.ts
+++ backend/src/lists/logic/get-lists/logic.ts
@@ -1,5 +1,5 @@
 import {Id} from '../../../id/types';
-import {List} from '../../models/types';
+import {List, ListItem} from '../../models/types';
 import {Build} from '../types';
 
 export type GetLists = (_: {listId?: Id; ownerId?: Id}) => Promise<List[]>;
@@ -8,18 +8,48 @@ export const buildGetLists: Build<GetLists> = ({
   unitOfWork: {Lists, ListItems},
   TMDbLogic,
 }) => {
+  const aggergateListItem = async (listItem: ListItem) => {
+    const tmdbData = await TMDbLogic.request({
+      path: `/${listItem.tmdbMediaType}/${listItem.tmdbMediaId}`,
+    });
+    return {
+      ...listItem,
+      tmdbData,
+    };
+  };
+
+  const aggergateListItems = (listItems: ListItem[]) =>
+    Promise.all(listItems.map(aggergateListItem));
+
+  const aggergateList = async (list: List) => {
+    const listItemCount = await ListItems.count({listId: list.id});
+    const listItems = await ListItems.find({listId: list.id});
+    const aggergatedListItems = await aggergateListItems(listItems);
+    return {
+      listItems: aggergatedListItems,
+      listItemCount,
+      ...list,
+    };
+  };
+
+  const aggergateLists = (lists: List[]) =>
+    Promise.all(lists.map(aggergateList));
+
   return async ({ownerId, listId}) => {
     if (listId) {
       const lists = await Lists.find({
         id: listId,
       });
-      return lists;
+      return aggergateLists(lists);
     }
 
-    const lists = await Lists.find({
-      ownerId,
-    });
+    if (ownerId) {
+      const lists = await Lists.find({
+        ownerId,
+      });
+      return aggergateLists(lists);
+    }
 
-    return lists;
+    return [];
   };
 };
--- backend/src/lists/logic/get-lists/logic.unit.test.ts
+++ backend/src/lists/logic/get-lists/logic.unit.test.ts
@@ -1,3 +1,40 @@
+import {buildListLogicFake} from '../build.fake';
+import {makeUserFake} from '../../../users/models/make-user.fake';
+
 describe('getting lists', () => {
-  it.todo('gets lists');
+  it('gets aggergated lists by ownerId or listId', async () => {
+    const {ListLogic} = buildListLogicFake();
+    const user = makeUserFake();
+    const [list] = await ListLogic.addLists([
+      {
+        ownerId: user.id,
+        title: 'my list',
+      },
+    ]);
+    const [listItem] = await ListLogic.addListItems([
+      {
+        listId: list.id,
+        tmdbMediaId: '550',
+        tmdbMediaType: 'movie',
+      },
+    ]);
+
+    const [aggergatedList1] = await ListLogic.getLists({listId: list.id});
+    const [aggergatedList2] = await ListLogic.getLists({ownerId: user.id});
+
+    expect(aggergatedList1).toStrictEqual(aggergatedList2);
+
+    expect(aggergatedList1).toEqual(
+      expect.objectContaining({
+        ...list,
+        listItemCount: expect.any(Number),
+        listItems: expect.arrayContaining([
+          expect.objectContaining({
+            ...listItem,
+            tmdbData: expect.any(Object),
+          }),
+        ]),
+      })
+    );
+  });
 });
