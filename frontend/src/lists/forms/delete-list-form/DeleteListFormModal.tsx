1ec9e5b0ca6496f00dc4d6b1951bc8e058798ac7
Christopher Vouga <crvouga@gmail.com>
2020-11-17 23:12:20 -0700

    🗑delete form

--- frontend/src/lists/forms/delete-list-form/DeleteListFormModal.tsx
+++ frontend/src/lists/forms/delete-list-form/DeleteListFormModal.tsx
@@ -1,15 +1,64 @@
-import { Dialog } from "@material-ui/core";
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
 import React from "react";
+import { useHistory } from "react-router";
 import { ZoomIn } from "../../../common/components/TransitionComponents";
+import useBoolean from "../../../common/hooks/useBoolean";
 import useModal from "../../../navigation/modals/useModal";
-import DeleteListForm from "./DeleteListForm";
+import { useListener } from "../../../utils";
+import useDeleteListForm from "./useDeleteListForm";
 
+const LoadingDialog = () => {
+  const { eventEmitter } = useDeleteListForm();
+  const isLoading = useBoolean(false);
+  useListener(eventEmitter, "submit", isLoading.setTrue);
+  useListener(eventEmitter, "submitSettled", isLoading.setFalse);
+  return (
+    <Dialog open={isLoading.value}>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <CircularProgress disableShrink />
+          </ListItemIcon>
+          <ListItemText primary="Deleting" />
+        </ListItem>
+      </List>
+    </Dialog>
+  );
+};
 export default () => {
+  const history = useHistory();
   const { isOpen, close } = useModal("DeleteListForm");
+  const { listId, submit, eventEmitter } = useDeleteListForm();
+
+  const handleSubmit = () => {
+    if (listId) {
+      submit({ listId });
+    }
+  };
+
+  useListener(eventEmitter, "submitSuccess", () => {
+    close();
+    history.push("/user");
+  });
 
   return (
     <Dialog TransitionComponent={ZoomIn} open={isOpen} onClose={close}>
-      <DeleteListForm onCancel={close} />
+      <LoadingDialog />
+      <DialogTitle>Delete list?</DialogTitle>
+      <DialogActions>
+        <Button onClick={close}>Cancel</Button>
+        <Button onClick={handleSubmit}>Delete</Button>
+      </DialogActions>
     </Dialog>
   );
 };
