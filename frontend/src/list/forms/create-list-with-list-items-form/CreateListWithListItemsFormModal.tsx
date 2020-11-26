8631df770e4c9dd05ad92a3885a369441d8b3549
Christopher Vouga <crvouga@gmail.com>
2020-11-25 22:34:48 -0700

    👨‍💻better feedback when creating list with list item

--- frontend/src/list/forms/create-list-with-list-items-form/CreateListWithListItemsFormModal.tsx
+++ frontend/src/list/forms/create-list-with-list-items-form/CreateListWithListItemsFormModal.tsx
@@ -9,12 +9,27 @@ import {
   TextField,
 } from "@material-ui/core";
 import React, { useRef } from "react";
-import { MovieCardHeaderContainer } from "../../../movie/components/MovieCardHeader";
 import useModal from "../../../app/modals/useModal";
 import { useSnackbar } from "../../../app/snackbar/redux/snackbar";
 import { LinkButton } from "../../../app/snackbar/Snackbar";
+import LoadingDialog from "../../../common/components/LoadingDialog";
+import useBoolean from "../../../common/hooks/useBoolean";
 import { useListener } from "../../../common/utility";
+import { MovieCardHeaderContainer } from "../../../movie/components/MovieCardHeader";
 import useCreateListWithListItemsForm from "./useCreateListWithListItemsForm";
+
+const Creating = () => {
+  const { eventEmitter } = useCreateListWithListItemsForm();
+  const isLoading = useBoolean(false);
+  useListener(eventEmitter, "submit", isLoading.setTrue);
+  useListener(eventEmitter, "submitSettled", isLoading.setFalse);
+  return (
+    <LoadingDialog
+      open={isLoading.value}
+      ListItemTextProps={{ primary: "Creating" }}
+    />
+  );
+};
 
 export default () => {
   const { isOpen, close } = useModal("CreateListWithListItemsForm");
@@ -53,37 +68,40 @@ export default () => {
   });
 
   return (
-    <Dialog open={isOpen} onClose={handleClose}>
-      <DialogTitle>Create List</DialogTitle>
-      <DialogContent>
-        <Box paddingBottom={1}>
-          <TextField
-            variant="outlined"
-            name="title"
-            label="Title"
-            placeholder="My List"
-            fullWidth
-            inputRef={refTitle}
-          />
-        </Box>
-        <List>
-          {mediaIds.map((mediaId) => (
-            <MovieCardHeaderContainer
-              key={mediaId.tmdbMediaId}
-              mediaId={mediaId}
+    <React.Fragment>
+      <Dialog open={isOpen} onClose={handleClose}>
+        <DialogTitle>Create List</DialogTitle>
+        <DialogContent>
+          <Box paddingBottom={1}>
+            <TextField
+              variant="outlined"
+              name="title"
+              label="Title"
+              placeholder="My List"
+              fullWidth
+              inputRef={refTitle}
             />
-          ))}
-        </List>
-      </DialogContent>
-      <DialogActions>
-        <Button size="large" onClick={handleClose}>
-          Cancel
-        </Button>
+          </Box>
+          <List>
+            {mediaIds.map((mediaId) => (
+              <MovieCardHeaderContainer
+                key={mediaId.tmdbMediaId}
+                mediaId={mediaId}
+              />
+            ))}
+          </List>
+        </DialogContent>
+        <DialogActions>
+          <Button size="large" onClick={handleClose}>
+            Cancel
+          </Button>
 
-        <Button size="large" onClick={handleSubmit}>
-          Create
-        </Button>
-      </DialogActions>
-    </Dialog>
+          <Button size="large" onClick={handleSubmit}>
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Creating />
+    </React.Fragment>
   );
 };
