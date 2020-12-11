c9144f72af8c434ae4a5100b17c7e7dc654f339c
Christopher Vouga <crvouga@gmail.com>
2020-12-11 15:38:13 -0700

    added filter to user page list section

--- frontend/src/user/TabPanelList.tsx
+++ frontend/src/user/TabPanelList.tsx
@@ -1,31 +1,111 @@
-import { Box, Typography } from "@material-ui/core";
-import React from "react";
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+  Chip,
+} from "@material-ui/core";
+import FilterListOutlinedIcon from "@material-ui/icons/FilterListOutlined";
+import React, { useState } from "react";
 import {
   AutoListCardGridContainer,
   ListCardGridContainer,
 } from "../list/lists/card/ListCardGrid";
 import { UserAggergation } from "./query";
+import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
+enum Filter {
+  Owner = "Owner",
+  Editor = "Editor",
+}
 
 export default ({ user }: { user: UserAggergation }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [selectedFilter, setSelectedFilter] = useState<null | Filter>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <React.Fragment>
       <Box p={2}>
         <Typography variant="h5" gutterBottom>
           Auto Lists
         </Typography>
+
         <AutoListCardGridContainer
           ownerId={user.user.id}
           count={user.autoListCount}
+          ItemProps={{ xs: 12 }}
         />
       </Box>
       <Box p={2}>
-        <Typography variant="h5" gutterBottom>
-          Lists
-        </Typography>
+        <Box display="flex" alignItems="center" paddingBottom={1}>
+          <Box flex={1} display="flex" alignItems="center">
+            <Box marginRight={1}>
+              <Typography variant="h5">Lists</Typography>
+            </Box>
 
+            {selectedFilter !== null && (
+              <Chip
+                size="small"
+                clickable
+                variant="outlined"
+                label={selectedFilter}
+                onClick={() => {
+                  setSelectedFilter(null);
+                }}
+                onDelete={() => {
+                  setSelectedFilter(null);
+                }}
+              />
+            )}
+          </Box>
+          <IconButton onClick={handleClick}>
+            <FilterListOutlinedIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            {Object.values(Filter).map((filter) => (
+              <MenuItem
+                key={filter}
+                selected={filter === selectedFilter}
+                onClick={() => {
+                  setSelectedFilter((selectedFilter) =>
+                    selectedFilter === filter ? null : filter
+                  );
+                  handleClose();
+                }}
+              >
+                {filter}
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
         <ListCardGridContainer
-          GetListParams={{ userId: user.user.id }}
-          count={user.listCount}
+          GetListParams={
+            selectedFilter === Filter.Editor
+              ? { editorId: user.user.id }
+              : selectedFilter === Filter.Owner
+              ? { ownerId: user.user.id }
+              : { userId: user.user.id }
+          }
+          count={
+            selectedFilter === Filter.Editor
+              ? user.editorListCount
+              : selectedFilter === Filter.Owner
+              ? user.ownerListCount
+              : user.listCount
+          }
         />
       </Box>
     </React.Fragment>
