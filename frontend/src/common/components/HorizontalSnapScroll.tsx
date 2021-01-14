5099a64a92e72eef1b96c6819707082588eb2a63
Christopher Vouga <crvouga@gmail.com>
2021-01-13 21:46:51 -0700

    added scroll buttons

--- frontend/src/common/components/HorizontalSnapScroll.tsx
+++ frontend/src/common/components/HorizontalSnapScroll.tsx
@@ -1,8 +1,12 @@
-import { Box, BoxProps, makeStyles } from "@material-ui/core";
-import clsx from "clsx";
-import React from "react";
+import { Box, BoxProps, Fab, makeStyles, Grow } from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import React, { useRef, useEffect, useState } from "react";
+import useBoolean from "../hooks/useBoolean";
 
-const useStyles = makeStyles({
+const ICON_BUTTON_HEIGHT = "24px";
+
+const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexDirection: "row",
@@ -18,16 +22,88 @@ const useStyles = makeStyles({
   childWrapper: {
     scrollSnapAlign: "start",
   },
-});
+  backButton: {
+    zIndex: 3,
+    position: "absolute",
+    top: `calc(50% - ${ICON_BUTTON_HEIGHT})`,
+    left: theme.spacing(2),
+  },
+  forwardButton: {
+    zIndex: 3,
+    position: "absolute",
+    top: `calc(50% - ${ICON_BUTTON_HEIGHT})`,
+    right: theme.spacing(2),
+  },
+}));
 
-export default ({ className, children, ...props }: BoxProps) => {
+export default ({ children, ...props }: BoxProps) => {
   const classes = useStyles();
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [leftDisabled, setLeftDisabled] = useState(true);
+  const [rightDisabled, setRightDisabled] = useState(true);
+
+  const updatedDisabled = () => {
+    if (ref.current) {
+      setLeftDisabled(ref.current.scrollLeft === 0);
+
+      setRightDisabled(
+        ref.current.scrollWidth === ref.current.getBoundingClientRect().width
+      );
+    }
+  };
+
+  const scrollLeft = () => {
+    if (ref.current) {
+      ref.current.scroll({
+        left: Math.max(
+          0,
+          ref.current.scrollLeft - ref.current.getBoundingClientRect().width
+        ),
+        behavior: "smooth",
+      });
+      updatedDisabled();
+    }
+  };
+
+  const scrollRight = () => {
+    if (ref.current) {
+      ref.current.scroll({
+        left:
+          ref.current.scrollLeft + ref.current.getBoundingClientRect().width,
+        behavior: "smooth",
+      });
+      updatedDisabled();
+    }
+  };
+
+  useEffect(() => {
+    updatedDisabled();
+  }, [ref.current]);
 
   return (
-    <Box className={clsx(classes.root, className)} {...props}>
-      {React.Children.map(children, (child) => (
-        <Box className={classes.childWrapper}>{child}</Box>
-      ))}
+    <Box position="relative" {...props}>
+      <Fab
+        disabled={leftDisabled}
+        size="small"
+        className={classes.backButton}
+        onClick={scrollLeft}
+      >
+        <ArrowBackIcon />
+      </Fab>
+
+      <div ref={ref} className={classes.root}>
+        {React.Children.map(children, (child) => (
+          <Box className={classes.childWrapper}>{child}</Box>
+        ))}
+      </div>
+      <Fab
+        disabled={rightDisabled}
+        size="small"
+        className={classes.forwardButton}
+        onClick={scrollRight}
+      >
+        <ArrowForwardIcon />
+      </Fab>
     </Box>
   );
 };
