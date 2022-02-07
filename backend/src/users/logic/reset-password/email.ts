a065051a03d441136c0bf7bd752e928064131190
Christopher Vouga <crvouga@gmail.com>
2022-02-07 05:32:32 -0700

    email looking good

--- backend/src/users/logic/reset-password/email.ts
+++ backend/src/users/logic/reset-password/email.ts
@@ -3,19 +3,20 @@ import { Link } from "../../../common/utils";
 import { User } from "../../models";
 import { secrets } from "../../../config";
 
-export const makeResetEmailHtml = (resetPasswordLink: string) =>
-  `
-  <h1>
-    This is your pickflix password reset email.
-  </h1>
-  
-  <p>
-    <a href="${resetPasswordLink}">
-      <button>
-      Reset Your Password
-      </button>
-    </a>
-  </p>
+export const makeResetEmailHtml = (resetPasswordLink: string) => `
+  <div style="text-align: center; color: white; background-color: #0F0F0F; padding: 2em;">
+    <h1>
+      This is your <span style="font-weight: black; color: #28B4E4;">Pickflix</span> password reset email.
+    </h1>
+    
+    <p>
+      <a href="${resetPasswordLink}">
+        <button style="padding: 1em; font-size: 1em; font-weight: bold; background-color: #28B4E4; color: white; border: none; border-radius: 0.5em; cursor: pointer;">
+          RESET PASSWORD
+        </button>
+      </a>
+    </p>
+  </div>
 `;
 
 export const makeResetPasswordEmail = ({
@@ -27,6 +28,6 @@ export const makeResetPasswordEmail = ({
 }): Email => ({
   to: user.emailAddress,
   from: secrets.sendGridRegisteredEmailAddress,
-  subject: "Password Reset",
+  subject: "Pickflix Password Reset",
   html: makeResetEmailHtml(link),
 });
